6b3eb1d11f704ff27cfadf6b406e22624c86dd8f
Jay Kothari <kotharijay65@gmail.com>
2021-01-16 21:33:08 +0530

    Created basic layout

---
+++ app/src/main/java/com/example/woc30_android_mid_evaluation_jay_kothari/SpecificDay.java
@@ -0,0 +1,14 @@
+package com.example.woc30_android_mid_evaluation_jay_kothari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpecificDay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_specific_day);
+    }
+}
